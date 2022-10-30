0fa9c01ff4f34e21ef04d733cba120564bef5852
Vitric <94797143+vityya@users.noreply.github.com>
2022-10-30 10:11:32 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,15 @@
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    speed: 1000,
+    loop: true,
+  
+  
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.sleder__btn__next',
+      prevEl: '.sleder__btn__prev',
+    },
+  
+
+  });
